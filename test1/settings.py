46712f61283c9bad91285320e7088e39ed49e929
Ryan <ryan.abolfazl@gmail.com>
2025-05-25 00:42:03 +0330

    fixing errors2

--- test1/settings.py
+++ test1/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewaredjango',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
